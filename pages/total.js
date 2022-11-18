52b868a7c4fb98a9d367fd88a27ca5935b985b92
Cleberich <97978772+Cleberich@users.noreply.github.com>
2022-11-17 22:36:29 -0300

    tercer commit

--- pages/total.js
+++ pages/total.js
@@ -1,4 +1,4 @@
-import Layout from '../layout/layout'
+import Layout from '../layout/Layout'
 import { useEffect, useCallback, use } from 'react';
 import useQuiosco from '../hooks/useQuiosco';
 import { formatearDinero } from '../helpers';
